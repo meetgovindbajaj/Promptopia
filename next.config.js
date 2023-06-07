b520c036c9b6c3d8d7d5a7d106226ac5317dd4e3
Govind Bajaj <meetgovindbajaj@gmail.com>
2023-06-08 02:58:47 +0530

    updating project...

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverComponentsExternalPackages: ['mongoose'], isrMemoryCacheSize: 50 },
+  experimental: { serverComponentsExternalPackages: ['mongoose'] },
   images: { domains: ['lh3.googleusercontent.com', 'https://s3.scriptcdn.net'] },
   webpack(config) {
     config.experiments = {
