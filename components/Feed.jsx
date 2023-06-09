9c8bb18ea1f132abf12db5a78a4fb74e56770b99
Govind Bajaj <meetgovindbajaj@gmail.com>
2023-06-09 05:42:47 +0530

    updating projects...

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -25,7 +25,7 @@ const Feed = () => {
   const handleSearchChange = (e) => setSearchText(e.target.value);
   const handleTagClick = (tag) => setSearchText(tag);
   useEffect(() => {
-    fetch('/api/prompt/all', { cache: 'no-store', method: 'POST' })
+    fetch('/api/prompt/all', { method: 'POST' })
       .then((r) => r.json())
       .then((r) => {
         setPosts(r);
